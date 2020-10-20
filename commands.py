6b7e818500e11c37c56430a8569702b14c3ea047
Benji Sidi <benjisidi@gmail.com>
2020-10-20 22:24:37 +0100

    Initial commit

---
+++ commands.py
@@ -0,0 +1,71 @@
+import json
+import os
+import pprint
+import argparse
+from pathlib import Path
+
+def process_args():
+    parser = argparse.ArgumentParser(description="A utility for quicksave/load of directories")
+    parser.add_argument('-c', '--command', choices=["ls", "go", "save", "rm"], required=True)
+    parser.add_argument('-k', '--key')
+    args = parser.parse_args()
+    return args
+
+def get_portals(args):
+    with open(f'{args["PORTALS_ROOT"]}/portal-list') as f:
+        data = json.load(f)
+    return data
+
+def save_portals(data, args):
+    with open(f'{args["PORTALS_ROOT"]}/portal-list', 'w') as f:
+        json.dump(data, f)
+
+def ls(args):
+    data = get_portals(args)
+    pprint.pprint(data)
+
+def save(args):
+    cur_dir = os.getcwd()
+    data = get_portals(args)
+    data[args["key"]] = cur_dir
+    save_portals(data, args)
+    pass
+
+def go(args):
+    data = get_portals(args)
+    if (args["key"] not in data):
+        print(f'No entry defined for {args["key"]}.')
+        exit()
+     # We're gonna pass this to cd
+    print(data[args["key"]])
+
+def rm(args):
+    data = get_portals(args)
+    if (args["key"] not in data):
+        print(f'No entry defined for {args["key"]}.')
+        exit()
+    del data[args["key"]]
+    save_portals(data, args)
+
+if __name__ == "__main__":
+    data_file = os.getenv("PORTALS_ROOT")
+    if data_file is None:
+        print("Please define the PORTALS_ROOT environment variable")
+        exit()
+    portals_list = Path(data_file + '/portal-list')
+    if not (portals_list.is_file()):
+        with portals_list.open("w") as f:
+            f.write("{}")
+
+    args = vars(process_args())
+    switch = {
+        "go": go,
+        "rm": rm,
+        "save": save,
+        "ls": ls
+    }
+    args["PORTALS_ROOT"] = data_file
+    if (args["command"] in ["save", "go", "rm"] and args["key"] is None):
+        print("Please specify a key")
+        exit()
+    switch[args["command"]](args)
---
+++ readme.md
@@ -0,0 +1,51 @@
+# Folder Portals
+
+## What is this thing?
+
+`folder-portals` is a tiny utility that gives you four simple commands:
+
+- `save someKey` creates a "portal" to the current working directory saved under `someKey`
+- `goto someKey` retrieves the portal `someKey` and navigates there
+- `portals` prints a list of your portals and their associated directories
+- `rmportal someKey` deletes the portal associated with `someKey`
+
+## Ok, I want it. How do I install it?
+
+1. If you don't have python3, install python3. (Not sure? `python3 --version`.)
+
+2. **Check out folder-portals where you want it installed.** I like ~/.portals, but it's up to you.
+	
+	```bash
+	git clone https://github.com/benjisidi/folder-portals $HOME/.portals
+	```
+	
+3. **Define the environment variable PORTALS_ROOT** to wherever you cloned the repo to, and add `$PORTALS_ROOT/bin` to your `$PATH`.
+
+	- For **bash:**
+        ```bash
+        echo 'export PORTALS_ROOT="$HOME/.portals"' >> ~/.bash_profile
+        echo 'export PATH="$PORTALS_ROOT/bin:$PATH"' >> ~/.bash_profile
+        ```
+   - For **Ubuntu:**
+        ```bash
+        echo 'export PORTALS_ROOT="$HOME/.portals"' >> ~/.bashrc
+        echo 'export PATH="$PORTALS_ROOT/bin:$PATH"' >> ~/.bashrc
+        ```
+    - For **Zsh:**
+        ```bash
+        echo 'export PORTALS_ROOT="$HOME/.portals"' >> ~/.zshrc
+        echo 'export PATH="$PORTALS_ROOT/bin:$PATH"' >> ~/.zshrc
+        ```
+All going well, you should now be able to restart your shell with `$SHELL`, and running `portals` should print an empty map `{}`
+
+## Why's it a weird mishmash of python and shell script?
+
+- I hacked it together one afternoon because I use WSL2 on windows and having to navigate between Windows and Linux directories all the time was a pain. 
+
+- I then equally hackily modified it for general use.
+
+- Don't judge.
+
+
+
+- ...stay tuned for a more sensible version.
